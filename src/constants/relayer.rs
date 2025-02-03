df129c8f9bee168ad3f11554e6d671337b951a37
Zeljko <zeljko89markovic@gmail.com>
2025-02-03 11:46:20 +0100

    chore: refactor

--- src/constants/relayer.rs
+++ src/constants/relayer.rs
@@ -2,4 +2,4 @@
 //! These values are used to ensure relayers maintain sufficient funds for operation.
 pub const DEFAULT_EVM_MIN_BALANCE: u128 = 1; // 0.001 ETH in wei
 pub const DEFAULT_STELLAR_MIN_BALANCE: u64 = 1_000_000; // 1 XLM
-pub const DEFAULT_SOLANA_MIN_BALANCE: u64 = 1_000_000_000; // 1 SOL
+pub const DEFAULT_SOLANA_MIN_BALANCE: u64 = 10_000_000; // 0.01 Lamport
