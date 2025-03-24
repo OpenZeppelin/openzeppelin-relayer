abffff956c7e18bc5243428e96cf2cb244e722db
Zeljko <zeljko89markovic@gmail.com>
2025-03-24 18:42:14 +0100

    chore: impr

--- helpers/generate_uuid.rs
+++ helpers/generate_uuid.rs
@@ -27,13 +27,13 @@ mod tests {
     #[test]
     fn test_uuid_generation() {
         let uuid_string = Uuid::new_v4().to_string();
-        
+
         let parsed_uuid = Uuid::from_str(&uuid_string);
         assert!(parsed_uuid.is_ok(), "Generated string is not a valid UUID");
-        
+
         let uuid = parsed_uuid.unwrap();
         assert_eq!(uuid.get_version_num(), 4, "UUID is not version 4");
-        
+
         let uuid1 = Uuid::new_v4();
         let uuid2 = Uuid::new_v4();
         assert_ne!(uuid1, uuid2, "Two generated UUIDs should not be equal");
