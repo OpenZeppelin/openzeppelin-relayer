1c0bdf5bdcef30a33f87e4fe558a384c3ffcb22b
Marcos <marcoscarlomagno1@gmail.com>
2025-06-11 12:05:47 -0300

    test: Add tests for plugin controller

--- src/api/controllers/plugin.rs
+++ src/api/controllers/plugin.rs
@@ -3,7 +3,7 @@
 //! Handles HTTP endpoints for plugin operations including:
 //! - Calling plugins
 use crate::{
-    jobs::JobProducer,
+    jobs::JobProducerTrait,
     models::{ApiError, ApiResponse, AppState, PluginCallRequest},
     services::plugins::{PluginService, PluginServiceTrait},
 };
@@ -21,10 +21,10 @@ use eyre::Result;
 /// # Returns
 ///
 /// The result of the plugin call.
-pub async fn call_plugin(
+pub async fn call_plugin<J: JobProducerTrait>(
     plugin_id: String,
     plugin_call_request: PluginCallRequest,
-    state: web::ThinData<AppState<JobProducer>>,
+    state: web::ThinData<AppState<J>>,
 ) -> Result<HttpResponse, ApiError> {
     let plugin = state
         .plugin_repository
@@ -38,4 +38,59 @@ pub async fn call_plugin(
         .await;
 
     Ok(HttpResponse::Ok().json(ApiResponse::success(result)))
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use super::*;
+    use crate::{
+        jobs::MockJobProducerTrait,
+        models::PluginModel,
+        repositories::{
+            InMemoryNetworkRepository, InMemoryNotificationRepository, InMemoryPluginRepository,
+            InMemoryRelayerRepository, InMemorySignerRepository, InMemoryTransactionCounter,
+            InMemoryTransactionRepository, PluginRepositoryTrait, RelayerRepositoryStorage,
+        },
+    };
+
+    async fn get_test_app_state() -> AppState<MockJobProducerTrait> {
+        // adds a custom plugin
+        let plugin_repository = InMemoryPluginRepository::new();
+        let plugin = PluginModel {
+            id: "test-plugin".to_string(),
+            path: "test-path".to_string(),
+        };
+        plugin_repository.add(plugin.clone()).await.unwrap();
+
+        AppState {
+            relayer_repository: Arc::new(RelayerRepositoryStorage::in_memory(
+                InMemoryRelayerRepository::new(),
+            )),
+            transaction_repository: Arc::new(InMemoryTransactionRepository::new()),
+            signer_repository: Arc::new(InMemorySignerRepository::new()),
+            notification_repository: Arc::new(InMemoryNotificationRepository::new()),
+            network_repository: Arc::new(InMemoryNetworkRepository::new()),
+            transaction_counter_store: Arc::new(InMemoryTransactionCounter::new()),
+            job_producer: Arc::new(MockJobProducerTrait::new()),
+            plugin_repository: Arc::new(plugin_repository),
+        }
+    }
+
+    #[actix_web::test]
+    async fn test_call_plugin() {
+        let app_state = get_test_app_state().await;
+        let plugin_call_request = PluginCallRequest {
+            plugin_id: "test-plugin".to_string(),
+            params: serde_json::json!({"key":"value"}),
+        };
+        let response = call_plugin(
+            "test-plugin".to_string(),
+            plugin_call_request,
+            web::ThinData(app_state),
+        )
+        .await;
+        assert!(response.is_ok());
+    }
 }
