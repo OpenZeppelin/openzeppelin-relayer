5d16238032a2569eb2847847ae5d2775752e6d7d
Luis Urrutia <luis@urrutia.me>
2025-08-19 12:09:20 +0200

    feat: Unify mempool detection

---
+++ src/constants/network_tags.rs
@@ -0,0 +1,5 @@
+/// Network tag constants for EVM networks
+pub const NO_MEMPOOL_TAG: &str = "no-mempool";
+pub const ARBITRUM_BASED_TAG: &str = "arbitrum-based";
+pub const OPTIMISM_BASED_TAG: &str = "optimism-based";
+pub const ROLLUP_TAG: &str = "rollup";
