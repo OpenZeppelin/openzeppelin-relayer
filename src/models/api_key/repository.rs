d616cadb7122ef3f84e89617a608c59b734ab702
Marcos <marcoscarlomagno1@gmail.com>
2025-08-28 10:57:21 -0300

    feat: Serialize and deserialize secret streing

--- src/models/api_key/repository.rs
+++ src/models/api_key/repository.rs
@@ -1,9 +1,16 @@
-use crate::models::SecretString;
+use crate::{
+    models::SecretString,
+    utils::{deserialize_secret_string, serialize_secret_string},
+};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ApiKeyRepoModel {
     pub id: String,
+    #[serde(
+        serialize_with = "serialize_secret_string",
+        deserialize_with = "deserialize_secret_string"
+    )]
     pub value: SecretString,
     pub name: String,
     pub allowed_origins: Vec<String>,
