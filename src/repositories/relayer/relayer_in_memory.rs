eb734cddaec061efd8563d03f03b5caaf4e35257
tirumerla <tirumerla@gmail.com>
2026-05-07 16:49:11 -0700

    chore: Bump Rust toolchain to 1.91.1

--- src/repositories/relayer/relayer_in_memory.rs
+++ src/repositories/relayer/relayer_in_memory.rs
@@ -30,7 +30,7 @@ impl InMemoryRelayerRepository {
             store: Mutex::new(HashMap::new()),
         }
     }
-    async fn acquire_lock<T>(lock: &Mutex<T>) -> Result<MutexGuard<T>, RepositoryError> {
+    async fn acquire_lock<T>(lock: &Mutex<T>) -> Result<MutexGuard<'_, T>, RepositoryError> {
         Ok(lock.lock().await)
     }
 }
